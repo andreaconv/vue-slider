c419c71c1210a18962873f0aa22f61e2cd08b8e3
andreaconv <andrea2convertini@gmail.com>
2023-04-13 16:07:33 +0200

    [edit]  operatore ternario

--- JS/scipt.js
+++ JS/scipt.js
@@ -19,8 +19,8 @@ createApp({
 
   methods: {
     button(isNext){
-      if(isNext) this.counter++;
-      else this.counter--
+      isNext ? this.counter++ : this.counter--;
+
       if(this.counter === this.images.length) this.counter = 0;
       if(this.counter < 0) this.counter = this.images.length - 1;
       console.log(this.counter)
